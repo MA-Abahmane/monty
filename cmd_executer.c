44d5b809bdc47fea2b6a2adb28ab7dbb589e812a
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-25 20:33:40 +0000

    fixing: Errors

--- cmd_executer.c
+++ cmd_executer.c
@@ -32,7 +32,7 @@ char *oper;
 oper = strtok(Ldata, SEPARATORS);
 data.token = strtok(NULL, SEPARATORS);
 i = 0;
-while (oper && matcher[i].opcode && strcmp(oper, "#"))
+while (oper && strcmp(oper, "#") && matcher[i].opcode)
 {
 if (strcmp(oper, matcher[i].opcode) == 0)
 {
