0bde251df51e32a0b8d195e8984965646d93497e
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-25 14:37:01 +0100

    monty 2.0

--- monty.h
+++ monty.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /* Given data structures for this project */
 
