de95b55c7d9a4512afce451ada4483cffbbb7cc1
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-25 16:45:18 +0100

    monty 2.5

---
+++ cmd_executer.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+#define SEPARATORS " \n\t"
+
+int cmd_executer(FILE *file, char *Ldata, stack_t **stack, unsigned int count)
+{
+
+/* matching list (match the operator with its function) */
+instruction_t matcher[] = {
+{"push", _push},
+{"pall", _pall},
+{"pint", _pint},
+{"pop", _pop},
+{"swap", _swap},
+{"add", _add},
+{"nop", _nop},
+{"sub", _sub},
+{"div", _div},
+{"mul", _mul},
+{NULL, NULL}
+};
+
+unsigned int i;
+char *oper;
+
+/* get the operators from the given string using strtok() */
+oper = strtok(Ldata, SEPARATORS);
+/* save the operators argument for future use */
+data.token = strtok(NULL, SEPARATORS);
+i = 0;
+while (oper && matcher[i].opcode)
+{
+if (strcmp(oper, matcher[i].opcode) == 0)
+{
+matcher[i].f(stack, count);
+return (0);
+}
+
+i++; 
+}
+
+/* operators not found */
+if (oper && matcher[i].opcode == NULL)
+{
+fprintf(stderr, "L%d: unknown instruction %s\n", count, oper);
+fclose(file);
+free(Ldata);
+exit(EXIT_FAILURE);
+}
+
+
+return (1);
+}
---
+++ main.c
@@ -0,0 +1,59 @@
+#include "monty.h"
+
+/* information collector */
+e_info data = {NULL, NULL, NULL, 0};
+
+
+
+/**
+ * main - The code of the program
+ * @argv: main args array
+ * @argc: main args array
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+FILE *file;
+char *Ldata;
+size_t Lsize = 0;
+ssize_t crnt_line = 1;
+unsigned int count = 0;
+stack_t *stack = NULL;
+
+/* if more than one argument is given to the program */
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+
+/* open file to reading mode */
+file = fopen(argv[1], "r");
+data.file = file;
+/* if its not possible to open file */
+if (file == NULL)
+{
+  fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+  exit(EXIT_FAILURE);
+}
+
+/* get the commands from the .m file */
+while (crnt_line > 0)
+{
+Ldata = NULL;
+crnt_line = getline(&Ldata, &Lsize, file);
+data.content = Ldata;
+count++;
+/* send each command line to be executed */
+if (crnt_line > 0)
+{
+cmd_executer(file, Ldata, &stack ,count);
+}
+free(Ldata);
+}
+
+fclose(file);
+return (0);
+}
+
