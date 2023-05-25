26f8132842034b8bee7c9b00ba5897218953c8b5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-25 21:09:09 +0000

    fixing: Errors

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 /* information collector */
 e_info data = {NULL, NULL, NULL, 0};
 
-
-
 /**
  * main - The code of the program
  * @argv: main args array
  * @argc: main args array
- *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -20,21 +17,16 @@ size_t Lsize = 0;
 ssize_t crnt_line = 1;
 unsigned int count = 0;
 stack_t *stack = NULL;
-
-/* if more than one argument is given to the program */
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-
-/* open file to reading mode */
 file = fopen(argv[1], "r");
-/* if its not possible to open file */
 if (file == NULL)
 {
-  fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-  exit(EXIT_FAILURE);
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
 }
 data.file = file;
 while (crnt_line > 0)
@@ -43,15 +35,13 @@ Ldata = NULL;
 crnt_line = getline(&Ldata, &Lsize, file);
 data.content = Ldata;
 count++;
-/* send each command line to be executed */
 if (crnt_line > 0 && data.content[0] != '#')
 {
 cmd_executer(file, Ldata, &stack, count);
 }
 free(Ldata);
 }
-
 fclose(file);
+free_dlistint(stack);
 return (0);
 }
-
