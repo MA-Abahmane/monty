4cb9500f76423173327e5ca6bf1381c9e93c117a
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-25 18:22:22 +0000

    fixing: Errors

--- main.c
+++ main.c
@@ -3,40 +3,57 @@
 /* information collector */
 e_info data = {NULL, NULL, NULL, 0};
 
+
+
 /**
- * main - The program entry point
- * @argc: Arguments count
- * @argv: Arguments
- * Return: 0 on success or 1
+ * main - The code of the program
+ * @argv: main args array
+ * @argc: main args array
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
 FILE *file;
-char *Ldata = NULL;
+char *Ldata;
 size_t Lsize = 0;
+ssize_t crnt_line = 1;
 unsigned int count = 0;
 stack_t *stack = NULL;
 
+/* if more than one argument is given to the program */
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
+
+/* open file to reading mode */
 file = fopen(argv[1], "r");
+data.file = file;
+/* if its not possible to open file */
 if (file == NULL)
 {
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE);
+  fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+  exit(EXIT_FAILURE);
 }
-data.file = file;
-while (getline(&Ldata, &Lsize, file) != -1)
+
+/* get the commands from the .m file */
+while (crnt_line > 0)
 {
+Ldata = NULL;
+crnt_line = getline(&Ldata, &Lsize, file);
 data.content = Ldata;
 count++;
-cmd_executer(file, Ldata, &stack, count);
+/* send each command line to be executed */
+if (crnt_line > 0)
+{
+cmd_executer(file, Ldata, &stack ,count);
 }
 free(Ldata);
-if (fclose(file))
-    exit(-1);
+}
+
+fclose(file);
 return (0);
 }
+
